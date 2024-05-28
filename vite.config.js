c17e0e3f04e73138ffa16020a174e99ae04991aa
Jarrad L <jlovelace2500k@gmail.com>
2024-05-28 00:10:42 -0700

    third commit

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,5 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: "https://whoremonger.github.io/markdown-previewer/"
+  base: "https://whoremonger.github.io/markdown-previewer/",
 })
